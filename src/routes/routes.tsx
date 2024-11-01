4be1d9684608ce3a99d9e90aa20d01bb4eddc5fb
psycho hero <mehdi@hero.best>
2024-11-01 14:02:04 +0330

    fix conflict

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,32 +1,27 @@
-import {createBrowserRouter} from "react-router-dom";
-import {MainLayout} from "../layouts/MainLayout/MainLayout.tsx";
-import {WalletProcess} from "../components/ui/WalletProcess/WalletProcess.tsx";
-<<<<<<< HEAD
-import {Wallet} from "../components/ui/Wallet/Wallet.tsx";
-=======
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "../layouts/MainLayout/MainLayout.tsx";
+import { WalletProcess } from "../components/ui/WalletProcess/WalletProcess.tsx";
+import { Wallet } from "../components/ui/Wallet/Wallet.tsx";
 import TestPage from "../testPage.tsx";
->>>>>>> b266a54 (add balances api)
-
 
 const routes = createBrowserRouter([
-    {
-        element: <MainLayout/>,
-        children: [
-            {
-                path: "/",
-                element: <WalletProcess/>,
-            },
-            {
-<<<<<<< HEAD
-                path: "/wallet",
-                element: <Wallet/>,
-=======
-                path: "/test",
-                element: <TestPage/>,
->>>>>>> b266a54 (add balances api)
-            },
-        ],
-    },
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <WalletProcess />,
+      },
+      {
+        path: "/wallet",
+        element: <Wallet />,
+      },
+      {
+        path: "/test",
+        element: <TestPage />,
+      },
+    ],
+  },
 ]);
 
 export default routes;
